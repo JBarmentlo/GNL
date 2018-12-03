3461949ee7f0be5e0a1deedb2f0ef69da1099c92
Joep BARMENTLO <jbarment@e1r2p5.42.fr>
2018-12-03 14:54:31 +0100

    hohoho

--- get_next_line.h
+++ get_next_line.h
@@ -1,12 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbarment <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/12/03 14:51:09 by jbarment          #+#    #+#             */
+/*   Updated: 2018/12/03 14:52:16 by jbarment         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # define BUFF_SIZE 1000
 
-typedef struct  s_id{
-    int                 fd;
-    char                *str;
-}               t_id;
-
-int get_next_line(int fd, char **out);
+int				get_next_line(int fd, char **out);
 
 #endif
